4eb4b1b2246c1c8db34b7cdfc4f44f9869ac2795
xavelo <xavelo@gmail.com>
2024-09-07 11:57:24 +0200

    cleanup

--- src/main/java/com/xavelo/kafka/KafkaConsumerController.java
+++ src/main/java/com/xavelo/kafka/KafkaConsumerController.java
@@ -1,14 +1,9 @@
 package com.xavelo.kafka;
 
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.info.GitProperties;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
