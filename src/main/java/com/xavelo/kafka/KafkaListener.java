88e79b06d11712f92ced8b845cb65e9b8812f1b6
= <=>
2024-10-05 22:25:35 +0200

    json message

--- src/main/java/com/xavelo/kafka/KafkaListener.java
+++ src/main/java/com/xavelo/kafka/KafkaListener.java
@@ -2,27 +2,33 @@ package com.xavelo.kafka;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
 public class KafkaListener {
 
     private static final Logger logger = LoggerFactory.getLogger(KafkaListener.class);
 
-    @Autowired
-    private MongoTemplate mongoTemplate;
+    private final MongoTemplate mongoTemplate;
+    private final ObjectMapper objectMapper;
+
+    public KafkaListener(MongoTemplate mongoTemplate, ObjectMapper objectMapper) {
+        this.mongoTemplate = mongoTemplate;
+        this.objectMapper = objectMapper;
+    }
 
     @org.springframework.kafka.annotation.KafkaListener(topics = "test-topic", groupId = "test-group", containerFactory = "kafkaListenerContainerFactory")
-    public void consume(String message) {
-        logger.info("Received message: {}", message);        
+    public void consume(String message) throws JsonProcessingException {
+        logger.info("Received message: {}", message);   
+        Message json = objectMapper.readValue(message, Message.class);     
         checkCollection();
-        saveMessage(message);
-        Message msg = findMessageByKey("key");
-        logger.info("Read message: {}", msg.getValue());
+        saveMessage(json);
+        findMessageByKey(json.getKey());
     }
 
     private void checkCollection() {
@@ -34,10 +40,9 @@ public class KafkaListener {
         }
     }
 
-    private void saveMessage(String message) {
-        Message msg = new Message("key", message);
-        mongoTemplate.save(msg, "test_topic");
-        logger.info("Message with key {} saved",  msg.getKey());
+    private void saveMessage(Message message) {
+        mongoTemplate.save(message, "test_topic");
+        logger.info("Message with key {} saved",  message.getKey());
     }
 
     private Message findMessageByKey(String key) {
--- src/main/java/com/xavelo/kafka/Message.java
+++ src/main/java/com/xavelo/kafka/Message.java
@@ -1,12 +1,16 @@
 package com.xavelo.kafka;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class Message {
+    @JsonProperty
     private String key;
+    @JsonProperty
     private String value;
 }
 
