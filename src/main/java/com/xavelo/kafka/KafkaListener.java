18db2e971a29eba202b225f70db8fa31918e0f59
= <=>
2024-10-13 15:26:32 +0200

    DLQ

--- src/main/java/com/xavelo/kafka/KafkaListener.java
+++ src/main/java/com/xavelo/kafka/KafkaListener.java
@@ -2,12 +2,12 @@ package com.xavelo.kafka;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
+
 import org.springframework.stereotype.Service;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.springframework.kafka.core.KafkaTemplate;
 
 @Service
 public class KafkaListener {
@@ -16,20 +16,40 @@ public class KafkaListener {
 
     private final MongoAdapter mongoAdapter;
     private final ObjectMapper objectMapper;
+    private final KafkaTemplate<String, String> kafkaTemplate;
 
-    public KafkaListener(MongoAdapter mongoAdapter, ObjectMapper objectMapper) {
+    public KafkaListener(MongoAdapter mongoAdapter, ObjectMapper objectMapper, KafkaTemplate<String, String> kafkaTemplate) {
         this.mongoAdapter = mongoAdapter;
         this.objectMapper = objectMapper;
+        this.kafkaTemplate = kafkaTemplate;
     }
 
     @org.springframework.kafka.annotation.KafkaListener(topics = "test-topic", groupId = "test-group", containerFactory = "kafkaListenerContainerFactory")
-    public void consume(String message) throws JsonProcessingException {
-        logger.info("Received message: {}", message);   
-        Message json = objectMapper.readValue(message, Message.class);     
-        mongoAdapter.checkCollection();
-        mongoAdapter.saveMessage(json);
-        mongoAdapter.findMessageByKey(json.getKey());
+    public void consume(String message) {
+        logger.info("Received message: {}", message);
+        process(message);
+        logger.info("message successfully processed");
+    }
+
+    // dummy processor to simple parse JSON messages and send to DLQ in case of error
+    private void process(String message) {
+        try {
+            Message json = objectMapper.readValue(message, Message.class);
+
+            mongoAdapter.checkCollection();
+            mongoAdapter.saveMessage(json);
+            mongoAdapter.findMessageByKey(json.getKey());
+
+        } catch (JsonProcessingException e) {
+            logger.error("error {} processing message {}", e.getMessage(), message);
+            sendToDLQ(message);
+        }
+
+    }
+
+    // New method to send message to DLQ
+    private void sendToDLQ(String message) {       
+        kafkaTemplate.send("test-topic-dlq", message);
     }
 
 }
-
